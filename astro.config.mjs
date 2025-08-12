663fe2857562d74b4f5144c469f0930a9e220bac
danielpemor <dpm__10@hotmail.com>
2025-08-12 16:18:55 -0600

    Revert "Ingles y cambios varios"
    This reverts commit a1340d37a0aa7b6dc41e46f44704696e20930690.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,5 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-    i18n: {
-    defaultLocale: 'es',
-    locales: ['es', 'en'],
-    routing: {
-      prefixDefaultLocale: false
-    }
-  },
   integrations: [tailwind()],
 });
