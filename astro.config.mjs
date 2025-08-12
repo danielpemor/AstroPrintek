a1340d37a0aa7b6dc41e46f44704696e20930690
danielpemor <dpm__10@hotmail.com>
2025-08-12 16:09:33 -0600

    Ingles y cambios varios

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+    i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
   integrations: [tailwind()],
 });
