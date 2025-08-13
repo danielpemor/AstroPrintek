15961f299ce6cba38a751688bd6cc62e366a4367
danielpemor <dpm__10@hotmail.com>
2025-08-13 12:06:56 -0600

    mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,5 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  experimental: {
-    middleware: true
-  },
   integrations: [tailwind()],
 });
